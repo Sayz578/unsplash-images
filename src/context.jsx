fae914336c99cb48fc0b341227c3c6adba5fa8d6
Sayz578 <kamaevyaroslav8@gmail.com>
2025-09-24 14:56:17 +0300

    commit

--- src/context.jsx
+++ src/context.jsx
@@ -1,2 +1,19 @@
-import { createContext, useContext,useState } from "react";
-const 
+import { Children, createContext, useContext,useState } from "react";
+const AppContext = createContext()
+
+export const AppProvaider = children => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    const toggleDarkTheme = () => {
+        const newDarkTheme = !isDarkTheme
+        setIsDarkTheme(newDarkTheme)
+
+        document.body.classList.toggle("dark-theme", newDarkTheme)
+    }
+
+    return(
+        <AppContext.Provider value={{}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
