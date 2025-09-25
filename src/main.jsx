fdb8b432b255f64efba2fcd42b55b438632f862f
Sayz578 <kamaevyaroslav8@gmail.com>
2025-09-25 18:50:24 +0300

    commit
    add css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppProvaider } from './context.jsx'
-
+import "./index.css"
 
 const queryClient = new QueryClient()
 
