e3971def33e478620e3cb7378327c4477c1f1bd9
Sayz578 <kamaevyaroslav8@gmail.com>
2025-09-27 14:15:54 +0300

    commit
    add fix gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import { useGlobalContext } from "../context"
 import axios from "axios"
 
-const url = "https://api.unsplash.com/search/photos?client_id=p_PHLxFf-E-LLzcKzjnzgv-fsC_3-rJpQxDFmE4FTMI&query=wallpapers"
+const url = `https://api.unsplash.com/search/photos?client_id=p_PHLxFf-E-LLzcKzjnzgv-fsC_3-rJpQxDFmE4FTMI&query=${serchTherm}`
 // console.log(url);
 
 const Gallery = () => {
