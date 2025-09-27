c7f58ed8695aa9f0e29146ab39d1c00493e0f361
Sayz578 <kamaevyaroslav8@gmail.com>
2025-09-27 14:12:14 +0300

    commit
    add gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import { useGlobalContext } from "../context"
 import axios from "axios"
 
-const url = "https://api.unsplash.com/search/photos?client_id=p_PHLxFf-E-LLzcKzjnzgv-fsC_3-rJpQxDFmE4FTMI&query=dog"
+const url = "https://api.unsplash.com/search/photos?client_id=p_PHLxFf-E-LLzcKzjnzgv-fsC_3-rJpQxDFmE4FTMI&query=wallpapers"
 // console.log(url);
 
 const Gallery = () => {
@@ -11,11 +11,11 @@ const Gallery = () => {
       queryKey: ["images", serchTherm],
       queryFn: async() => {
         const result = await axios.get(`${url}&query=${serchTherm}`)
-        // console.log(result.data);
-        return result.data
+        // console.log(result.data.results);
+        return result.data.results
       }
     })
-    console.log(response);
+    // console.log(response);
     if (response.isLoading) {
       return(
         <section className="image-container">
@@ -34,8 +34,10 @@ const Gallery = () => {
         </section>
       )
     }
-    const resalts = response.data.resalts
-    if (resalts.lenght<1) {
+    const resalts = response.data
+    // console.log(response.data);
+    
+    if (resalts.length < 1) {
       return(
         <section className="image-container">
           <h4>
@@ -46,10 +48,12 @@ const Gallery = () => {
     }
   return (
     <section className="image-container">
-          <h4>
-            no resalts found...
-          </h4>
-        </section>
+          {resalts.map(item => {
+            // console.log(item?.urls?.regular);
+            const url = item?.urls?.regular
+            return <img src={url} key={item.id} alt={item.alt_description} className="img"/>
+          })}
+    </section>
   )
 }
 
